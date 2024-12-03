646b9ebb2999b54b14dfb2aa20d7476917d7d29c
Kyle McDowell <kjmcd03@gmail.com>
2024-12-02 21:50:00 -0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	
+  useEffect(() => {
+  setIsLoading(true);
+  try {
+      fetchTheData();
+    }
+  catch{setIsError(true)};
+}, []);
+
+   async function fetchTheData() {
+    const response=await fetch(`https://09kjtgt235.execute-api.us-east-2.amazonaws.com/dev/reading?id=dd2436ca-638d-4516-85b2-4de269157347&user=kay`);
+    const result=await response.json();
+    console.log(result);
+    setListItems(result);
+    setIsLoading(false);
+  }
+
+
+  // hook for the current list of items #1
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [listItems,setListItems]=useState([]);
+    const [inputValue,setInputValue]=useState('');
+function buttonHandler(){
+  try {
+    writeTheData(inputValue);
+    setInputValue('');
+    window.location.reload();
+  } catch (error) {
+   console.error("eat mah nutz");  
+  }
 }
+const handleTextChange = (event) => {
+  setInputValue(event.target.value);
+};
+
+async function writeTheData(item){
+  const encodeItem=encodeURIComponent(item);
+  setIsLoading(true);
+  const response=await fetch(`https://s5j1zlnq76.execute-api.us-east-2.amazonaws.com/test/writing?user=kay&text=${encodeItem}`);
+  const result=await response.json();
+  console.log(result);
+  setIsLoading(false);
+} 
+  return (
+    <div>
+      <h3>Reading my Database</h3>
+      <input type='text'onChange={handleTextChange} value={inputValue} placeholder='enter shit here'></input>
+      <button onClick={buttonHandler}>ADD STUFF</button>
+    {isError && <p>Erroneous state ...</p>}
+    
+    {isLoading ? (
+        <p>fingering jeff bezos...</p>
+      ) :
+       
+      <ul>
+            {listItems.map((item)=>(<li key={item.id}>{item.text}</li>))}
+          </ul> 
+    }
+    </div>
+   );
+};
 
 export default App;
